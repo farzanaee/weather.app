a3261899682c7e2acc8dc2e0760c129a4f8fa4c6
farzanaee <89377080+farzanaee@users.noreply.github.com>
2021-11-14 20:31:52 +0330

    Update index.js
    edit js

--- index.js
+++ index.js
@@ -59,3 +59,30 @@ function getForecast(coordinates) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
+
+//unit conversions
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  let windElement = document.querySelector("#wind");
+
+  celsiusLink.classList.remove("active");
+  celsiusLink.classList.add("inactive");
+  fahrenheitLink.classList.remove("inactive");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+  windElement.innerHTML = `Wind: ${Math.round(celsiusWind * 0.621371)} mph`;
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  celsiusLink.classList.remove("inactive");
+  fahrenheitLink.classList.add("inactive");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = `${Math.round(celsiusWind)} km/h`;
+}
